fc4dd18111fe5c13554007025fde5791c4075108
tomf247 <tomfoley247@gmail.com>
2023-01-22 21:28:22 +0000

    Point Heroku to correct app

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','tf-ecommerce.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','tf-vittoria.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io','https://*.127.0.0.1']
 
 
