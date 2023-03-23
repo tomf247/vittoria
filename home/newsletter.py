b245badf97d0332077d9df58b7d50fb53a1230f9
tomf247 <tomfoley247@gmail.com>
2023-03-23 16:28:26 +0000

    Fix newsleter notification message

--- home/newsletter.py
+++ home/newsletter.py
@@ -6,25 +6,23 @@ from django.contrib import messages
 from email_validator import validate_email, EmailNotValidError
 
 def newsletter(request):
-    newsform = NewsletterForm()
-    news = Newsletter.objects.all()
+    news = Newsletter()
     if request.method=='POST':
         if request.POST.get("form_type") == 'newsletter':
-            newsform=NewsletterForm(request.POST)
-            if newsform.is_valid():
                 email = request.POST.get('email')
                 try:
                     is_new_account = True
                     validation = validate_email(email, check_deliverability=is_new_account)
-                    email = validation.email
+                    validation.email
                     send_mail(
                     subject=f'Newsletter Vittoria Ecommerce',
                     message=f'Hi {email}  Thanks for subscribing to our Newsletter, \n The Vittoria Ecommerce Team',
                     from_email=settings.EMAIL_HOST_USER,
                     recipient_list=[email]) #this email account receives the damage claim notification
-                    newsform.save()
+                    news.emails = email
+                    news.save()
                     messages.info(request,'Thank you for subscribing to our Newsletter')
-                except:
+                except EmailNotValidError:
                     messages.error(request,"Sorry your email is not valid")
 
-    return{'newsform':newsform}
+    return{'newsform':news}
